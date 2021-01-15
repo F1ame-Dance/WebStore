5ef9414793265a956c6d9717889f16f700d76c32
FlameDance <FlameDance@DESKTOP-16QNFGI>
2021-01-15 21:53:03 +0300

    Lesson1 results

---
+++ WebStore/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using WebStore.Models;
+
+namespace WebStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        private static readonly List<Employee> _Employees = new()
+        {
+            new Employee { Id = 1, LastName = "Иванов", FirstName = "Иван", Patronymic = "Иванович", Age = 22 },
+            new Employee { Id = 2, LastName = "Петров", FirstName = "Пётр", Patronymic = "Петрович", Age = 33 },
+            new Employee { Id = 3, LastName = "Сидоров", FirstName = "Сидор", Patronymic = "Сидорович", Age = 44 },
+        };
+
+        public IActionResult Index() => View();
+
+        public IActionResult SecondAction()
+        {
+            return Content("Second controller action");
+        }
+
+        public IActionResult Employees()
+        {
+            return View(_Employees);
+        }
+    }
+}
