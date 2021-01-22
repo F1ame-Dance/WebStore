e714e4cbaa47cc14cbc227e2c7b50e65c0b39eba
FlameDance <FlameDance@DESKTOP-16QNFGI>
2021-01-22 19:48:11 +0300

    Текст

--- WebStore/Startup.cs
+++ WebStore/Startup.cs
@@ -35,6 +35,7 @@ namespace WebStore
 
             app.UseEndpoints(endpoints =>
             {
+                // Проекция запроса на действие
                 endpoints.MapGet("/greetings", async context =>
                 {
                     await context.Response.WriteAsync(Configuration["Greetings"]);
